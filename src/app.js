e6a08cf7bfb6dbc100b62f85012e81fe66f6feea
Bharat Kumawat <bharatkumawatk1@gmail.com>
2024-10-10 00:49:22 +0530

    CRUD api for user , Schema validation, api level data sanitization

--- src/app.js
+++ src/app.js
@@ -3,13 +3,10 @@ const connectDB = require("./config/database");
 const User = require("./models/user");
 const app = express();
 
+app.use(express.json());
+
 app.post("/signup", async (req, res) => {
-  const user = new User({
-    firstName: "Bharat",
-    lastName: "Kumar",
-    emailId: "bharat@gmail.com",
-    password: "bharat123",
-  });
+  const user = new User(req.body);
 
   try {
     await user.save();
@@ -18,6 +15,65 @@ app.post("/signup", async (req, res) => {
     res.status(400).send("Error saving the user:" + err.message);
   }
 });
+
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.emailId;
+  try {
+    const user = await User.findOne({emailId: userEmail});
+    if(user){
+      res.send(user);
+    }
+    else{
+      res.status(404).send("user not found");
+    }
+    
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+})
+
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+app.delete("/user", async (req, res) => {
+  const userId = req.body.userId;
+  try {
+    const user = await User.findByIdAndDelete(userId);
+    res.send("User deleted successfully");
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+app.patch("/user/:userId", async (req, res) => {
+  const userId = req.params?.userId;
+  const data = req.body;
+  try {
+    const ALLOWED_UPDATES = ["photoUrl", "about", "gender", "age", "skills"];
+    const isUpdateAllowed = Object.keys(data).every((k) => 
+      ALLOWED_UPDATES.includes(k)
+    );
+    if(!isUpdateAllowed){
+      throw new  Error("Update not allowed");
+    }
+    if (data.skills && data.skills.length > 25) {
+      throw new Error("skills cannot be more than 25");
+    }
+    // if(data?.skills.length > 25){
+    //   throw new Error("skills cannot be more than 25")
+    // }
+    await User.findByIdAndUpdate(userId, data, {runValidators: true});
+    res.send("User updated successfully");
+  } catch (error) {
+    res.status(400).send("Update failed:" + error.message);
+  }
+})
 
 connectDB()
   .then(() => {
--- src/models/user.js
+++ src/models/user.js
@@ -3,22 +3,59 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
   firstName: {
     type: String,
+    required: true,
+    minLength: 3,
+    maxLength: 50,
+    trim: true,
+
   },
   lastName: {
     type: String,
+    minLength: 3,
+    maxLength: 50,
+    trim: true,
   },
   emailId: {
     type: String,
+    required: true,
+    minLength: 3,
+    maxLength: 50,
+    trim: true,
+    lowercase: true,
+    unique: true,
   },
   password: {
     type: String,
+    required: true,
+    minLength: 3,
+    maxLength: 50,
+    trim: true,
   },
   age: {
-    type: String,
+    type: Number,
+    min:  18,
+    required: true,
   },
   gender: {
     type: String,
+    validate(value) {
+      if(!["male", "female", "others"].includes(value)){
+        throw new Error("Gender data is not valid");
+      }
+    },
   },
+  photoUrl: {
+    type: String,
+    default: "https://geographyandyou.com/images/user-profile.png",
+  },
+  about: {
+    type: String,
+  },
+  skills: {
+    type: Array,
+  }
+},{
+  timestamps: true,
 });
 
 const User = mongoose.model("User", userSchema);
