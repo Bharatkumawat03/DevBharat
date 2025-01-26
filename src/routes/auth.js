e2fd1dcaf11cb4b8c2e617abab33f342112c59c8
Bharat Kumawat <bharatkumawatk1@gmail.com>
2025-01-27 00:32:02 +0530

    cokkie issue fix

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -68,6 +68,9 @@ authRouter.post("/login", async (req, res) => {
 authRouter.post("/logout", async (req, res) => {
     res.cookie("token", null, {
         expires: new Date(Date.now()),
+        httpOnly: true,
+        sameSite: "None",
+        secure: true,
     });
     res.send("Logout successfull!");
 });
