a9ad9bbd8293fe02c2ff6fc6e822f00279c1bf6f
Bharat Kumawat <bharatkumawatk1@gmail.com>
2025-02-03 01:25:45 +0530

    razorpay and webSocket

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const connectDB = require("./config/database");
 const app = express();
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
+const http = require("http");
 
 require("dotenv").config();
 
@@ -28,11 +29,19 @@ const authRouter = require("./routes/auth");
 const profileRouter = require("./routes/profile");
 const requestRouter = require("./routes/request");
 const userRouter = require("./routes/user");
+const paymentRouter = require("./routes/payment");
+const chatRouter = require("./routes/chat");
+const initializeSocket = require("./utils/socket");
 
 app.use("/", authRouter);
 app.use("/", profileRouter);
 app.use("/", requestRouter);
 app.use("/", userRouter);
+app.use("/", paymentRouter);
+app.use("/", chatRouter);
+
+const server = http.createServer(app);
+initializeSocket(server);
 
 connectDB()
 .then(() => {
@@ -40,7 +49,7 @@ connectDB()
     res.send("API WORKING");
   });
     console.log("Database connection established...");
-    app.listen(process.env.PORT, () => {
+    server.listen(process.env.PORT, () => {
       console.log("Server is listening on port 3000...");
     });
   })
